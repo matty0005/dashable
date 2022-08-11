b271db83fb27a3365d3d3eed73f8f12a1a3c6a94
Matthew Gilpin <matty0005@gmail.com>
2022-08-11 23:12:43 +1000

    Added live preview to dashboard page

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,12 +1,12 @@
 <template>
     <AppLayout title="Dashboard">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <h2 class="font-semibold text-xl text-gray-700 leading-tight">
                 Dashboard
             </h2>
         </template>
 
-        <div class="py-12">
+        <div class="py-12 text-gray-700" >
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
                     <div class="text-3xl">
@@ -15,6 +15,11 @@
                     
                     <div class="flex flex-row mt-8">
                         Link to your dashboard: <a :href="$page.props.dashboard_link" target="_blank" class="ml-2 text-indigo-600">{{$page.props.dashboard_link}}</a>
+                    </div>
+
+                    <div class="text-gray-600 text-xl mt-12">Live Preview:</div>
+                    <div class="bg-red-500 m-12 mb-12 my-4">
+                        <iframe id="scaled-frame" class="aspect-video w-full object-cover no-scrollbar" :src="$page.props.dashboard_link"/>
                     </div>
 
                     
@@ -57,3 +62,20 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+/* #scaled-frame {
+        zoom: 0.5;
+        -moz-transform: scale(0.5);
+        -moz-transform-origin: 0 0;
+        -o-transform: scale(0.5);
+        -o-transform-origin: 0 0;
+        -webkit-transform: scale(0.5);
+        -webkit-transform-origin: 0 0;
+      }
+      @media screen and (-webkit-min-device-pixel-ratio:0) {
+        #scaled-frame {
+          zoom: 1;
+        }
+      } */
+</style>
--- routes/web.php
+++ routes/web.php
@@ -28,17 +28,6 @@ Route::get('/', function () {
     ]);
 });
 
-// Route::middleware([
-//     'auth:sanctum',
-//     config('jetstream.auth_session'),
-//     'verified',
-// ])->group(function () {
-//     Route::get('/dashboard', function () {
-//         return Inertia::render('Dashboard');
-//     })->name('dashboard');
-// });
-
-
 Route::get('/dashboard/{dashboardId}', [Dashboard::class, 'index']);
 
 Route::get('/dashboard', [HomePageController::class, 'index'])->name('dashboard')->middleware('auth:sanctum', config('jetstream.auth_session'),'verified');
