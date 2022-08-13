aed92c10c9d9792f417996bf9f8ca14fc1ef7a27
Matthew Gilpin <matty0005@gmail.com>
2022-08-14 00:18:42 +1000

    Made text more readable accross backgrounds

--- resources/js/Pages/Public/Dashboard.vue
+++ resources/js/Pages/Public/Dashboard.vue
@@ -4,14 +4,14 @@
                 <WeatherRadar :images="radarImages" :radarId="config.weather_radar_id"/>
             </div> 
         <div class="flex flex-col">
-            <div class="text-10xl text-white  mt-12 ml-20">{{time}}</div>
+            <div class="text-10xl text-white  mt-12 ml-20 px-8 rounded-2xl backdrop-blur-md bg-black bg-opacity-30">{{time}}</div>
               
             <div class="flex-grow"></div>
            
             <div class="flex flex-row">
                  
 
-                <div class="text-6xl mb-8 ml-12 mt-12 h-32 w-72 flex rounded-md bg-gray-50 bg-opacity-30 relative">
+                <div class="text-6xl mb-8 ml-12 mt-12 h-32 w-72 flex rounded-md bg-black backdrop-blur-md bg-opacity-30 relative">
                     <div class="text-2xl text-white mt-1 ml-2 absolute">
                         {{weather.weather[0].main}}
                     </div>
@@ -22,7 +22,7 @@
 
                 
 
-                <div class="text-3xl mb-8 ml-12 mt-12 h-32 w-72 flex rounded-md bg-gray-50 bg-opacity-30 flex flex-col">
+                <div class="text-3xl mb-8 ml-12 mt-12 h-32 w-72 flex rounded-md bg-black backdrop-blur-md bg-opacity-30 flex flex-col">
                     <div class="text-white mt-auto mx-auto flex flex-row">
                         <div class="text-2xl text-bottom mt-1 mr-2">Feels like:</div> {{Math.round(weather.main.feels_like)}}°C
                     </div>
